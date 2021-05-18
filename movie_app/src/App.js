701de1e565794f38335b840eda5ad36695ff0d98
hyunjoebrother <hyunjoe31@gmail.com>
2021-05-19 05:00:46 +0900

    210519_ReactJS_study

---
+++ movie_app/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+class App extends React.Component {
+    state = {
+        isLoading : true,
+        movies : []
+    };
+
+    componentDidMount() {
+        setTimeout( () => {
+            this.setState({isLoading : false});
+        }, 6000);
+    }
+
+    render() {
+        const {isLoading} = this.state;
+
+        return (
+            <div>
+                {isLoading ? "Loading" : "We are ready"}
+            </div>
+        );
+    }
+}
+
+export default App;
--- movie_app/src/index.js
+++ movie_app/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 
 //import Potato from "./Potato";
 //import Appfood from './Appfood';
-import Appstate from './Appstate';
+//import Appstate from './Appstate';
+import App from './App';
 
 
 //ReactDOM.render(<Appfood />,document.getElementById('root'));
-ReactDOM.render(<Appstate />,document.getElementById('root'));
+//ReactDOM.render(<Appstate />,document.getElementById('root'));
+ReactDOM.render(<App />,document.getElementById('root'));
